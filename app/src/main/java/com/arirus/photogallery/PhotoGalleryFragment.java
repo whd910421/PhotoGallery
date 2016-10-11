c63ee3eb211f5daa863be7cf7c566eced61b1eb2
Arirus <whd910421@hotmail.com>
2016-10-11 12:19:29 +0800

    框架搭建

---
+++ app/src/main/java/com/arirus/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,43 @@
+package com.arirus.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by whd910421 on 16/10/11.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+    private final static String TAG = "PhotoGralleryFragemnet";
+
+    private RecyclerView mRecyclerView;
+
+    public static PhotoGalleryFragment newInstance()
+    {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mRecyclerView = (RecyclerView)v.findViewById(R.id.fragment_photo_gallery_recycle_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return v;
+    }
+
+
+}
