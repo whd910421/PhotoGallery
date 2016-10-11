6f3e165a81e68182f259d4e5cc3aafb96437579c
Arirus <whd910421@hotmail.com>
2016-10-11 16:15:00 +0800

    正确的收到json串

--- app/src/main/java/com/arirus/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/arirus/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.arirus.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by whd910421 on 16/10/11.
@@ -27,6 +30,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -39,5 +43,20 @@ public class PhotoGalleryFragment extends Fragment {
         return v;
     }
 
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... params) {
+//            try {
+//                String result = new FlickrFetchr().getUrlString("http://www.3dmgame.com");
+//                arirusLog.get().ShowLog(TAG,"成功获取", result);
+//            }catch (IOException ioe)
+//            {
+//                arirusLog.get().ShowLog(TAG, "获取失败" , ioe.toString());
+//            }
+            new FlickrFetchr().fetchItems();
+            return null;
+        }
+    }
 
 }
